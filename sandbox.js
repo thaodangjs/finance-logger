ee3e7720f6ecd4facf31ed2344321f13be107cd8
Thao Dang <thaodang.js@gmail.com>
2023-12-16 10:47:59 +0700

    finish lesson 2

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var greeting = "Hello";
+var inputs = document.querySelectorAll("input");
+console.log(greeting);
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+console.log("Good morning");
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+const greeting = "Hello";
+
+const inputs = document.querySelectorAll("input");
+
+console.log(greeting);
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
+
+console.log("Good morning");
